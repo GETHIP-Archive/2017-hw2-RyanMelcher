007a1ddf9236ed39087cb7dd6a857b4e1864d9f6
RyanMelcher <ryanmelcher12@gmail.com>
2016-10-01 13:12:50 -0500

    Completed homework assignment - Week 2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {
+  console.log("ready");
   $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-RyanMelcher/master/ryan.json",
   function(data)
   {
@@ -7,5 +8,9 @@ $(document).ready(function()
     $("#profile-name").text(data.fireName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
-  });
+  }).fail(function( jqxhr, textStatus, error ) {
+    var err = textStatus + ", " + error;
+    console.log( "Request Failed: " + err );
+});
+
 });
